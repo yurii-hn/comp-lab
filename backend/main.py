fe8869c29883e5c494742eaee3d2447548b2bc7d
why-not-dev <197524059+why-not-dev@users.noreply.github.com>
2023-05-24 15:38:00 +0300

    [Milestone] Model Data Processing + MATLAB Integration

---
+++ backend/main.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import json
+import matlab.engine
+import numpy as np
+
+eng = matlab.engine.start_matlab()
+eng.cd(r'../matlab', nargout=0)
+
+app = Flask(__name__)
+
+@app.route('/simulate', methods=['POST'])
+def simulate():
+    data = request.get_json()
+
+    result = eng.simulate(str(data).replace("'", '"'))
+
+    for i in range(len(result['compartments'])):
+        result['compartments'][i] = np.array(result['compartments'][i]).flatten().tolist()
+
+    string = str(result).replace("'", '"')
+
+    obj = json.loads(string)
+
+    return jsonify(obj)
+
+if __name__ == '__main__':
+    app.run(debug=True)
